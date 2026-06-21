9141ad794613988f2444bee1db86c161646b624b
Dana Taher <1221240@student.birzeit.edu>
2026-06-21 16:55:13 +0300

    Add event detail activity with fragment

--- app/src/main/java/com/example/a1221240_courseproject/EventDetailActivity.java
+++ app/src/main/java/com/example/a1221240_courseproject/EventDetailActivity.java
@@ -1,4 +1,48 @@
 package com.example.a1221240_courseproject;
 
-public class EventDetailActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class EventDetailActivity extends AppCompatActivity {
+
+    Button buttonBackFromDetail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_detail);
+
+        buttonBackFromDetail = (Button) findViewById(R.id.buttonBackFromDetail);
+
+        String title = getIntent().getStringExtra("title");
+        String description = getIntent().getStringExtra("description");
+        String category = getIntent().getStringExtra("category");
+        String date = getIntent().getStringExtra("date");
+        String time = getIntent().getStringExtra("time");
+        String location = getIntent().getStringExtra("location");
+        int seats = getIntent().getIntExtra("seats", 0);
+
+        EventDetailFragment fragment = EventDetailFragment.newInstance(
+                title, description, category, date, time, location, seats);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+        fragmentTransaction.commit();
+
+        buttonBackFromDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EventDetailActivity.this, EventListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 }
